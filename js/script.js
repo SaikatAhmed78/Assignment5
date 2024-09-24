9b248cd3b2dd25a968d9d3766ace7820bee510dd
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-09-25 00:14:04 +0600

    Assignment 5 Project Done

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+
+
+const modalSec = document.getElementById('section-modal');
+
+const donateBtn = document.getElementsByClassName('donate-now-btn');
+
+for (let donate of donateBtn) {
+    donate.addEventListener('click', function (event) {
+
+        const myBalance = getTextElementValueById('account');
+        const donateAmaount = parseFloat(event.target.parentNode.parentNode.childNodes[1].childNodes[3].innerText)
+        const inputAmount = parseFloat(event.target.parentNode.parentNode.childNodes[5].childNodes[1].value)
+       
+        if(isNaN(inputAmount) || myBalance < inputAmount || inputAmount <= 0 ){
+            alert('Invalid Amount')
+            return;
+        }
+        event.target.parentNode.parentNode.childNodes[1].childNodes[3].innerText = donateAmaount + inputAmount;
+
+        
+        console.log(myBalance)
+        document.getElementById('account').innerText = myBalance - inputAmount;
+
+        const donatePlace = event.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText 
+        modalSec.showModal()
+
+        event.target.parentNode.parentNode.childNodes[5].childNodes[1].value = ''
+        creatHistory(inputAmount, donatePlace)
+
+    })
+}
+
+
+
+
+document.getElementById('donation-button').addEventListener('click', function(){
+    console.log('hello')
+       showElement('donation-section')
+   })
+   
+   document.getElementById('history-button').addEventListener('click', function(){
+    console.log('done')
+       showElement('history-container')
+   })
+
+
+   document.getElementById('donation-button').addEventListener('click', function(){
+
+    document.getElementById('donation-button').classList.add('bg-[#B4F461]');
+    document.getElementById('history-button').classList.remove('bg-[#B4F461]');
+    
+   })
+ 
+   document.getElementById('history-button').addEventListener('click', function(){
+
+    document.getElementById('donation-button').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-button').classList.add('bg-[#B4F461]');
+    
+   })
+
+
+
---
+++ js/utilitis.js
@@ -0,0 +1,39 @@
+
+function getTextElementValueById(id) {
+    const elementValue = parseFloat(document.getElementById(id).innerText)
+    return elementValue;
+}
+
+
+function creatHistory(amount, placeName) {
+    const date = new Date();
+    let text = date.toString();
+
+    const historyContainer = document.getElementById('history-container');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="flex flex-col gap-5 items-start">
+        <div class="w-full bg-white shadow-lg rounded-lg p-6 border mb-2">
+          
+          <h1 class="text-xl font-semibold text-gray-800">
+              ${amount} Taka is ${placeName}
+          </h1>
+          <p class="text-sm text-gray-500 mt-3">
+              Date: ${text}
+          </p>
+      </div>
+      </div>
+    `
+
+    historyContainer.appendChild(div);
+
+}
+
+function showElement(id){
+    document.getElementById('history-container').classList.add('hidden')
+    document.getElementById(id).classList.remove('hidden')
+    document.getElementById('donation-section').classList.add('hidden')
+    document.getElementById(id).classList.remove('hidden')
+}
+
+
